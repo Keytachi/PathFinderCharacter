5f25b0d22a42cf8137c57bf8b71b7a79c5b4ebd7
Brandon <brandonle1992@gmail.com>
2016-12-27 21:11:37 -0600

    More class being added to this project

---
+++ src/com/brandontest/Controls/AI.java
@@ -0,0 +1,10 @@
+package com.brandontest.Controls;
+
+/**
+ * Created by Brandon on 12/27/2016.
+ */
+
+public class AI {
+
+    //TODO Create an AI system that has a logic if it knows it is going to fail then it will heal or flee.
+}
---
+++ src/com/brandontest/Weapons/Armor.java
@@ -0,0 +1,13 @@
+package com.brandontest.Weapons;
+
+/**
+ * Created by Brandon on 12/27/2016.
+ */
+public class Armor {
+
+    private int baseArmor;
+
+
+    //TODO: Create the armor class so that other piece can be inherit this class later on
+
+}
